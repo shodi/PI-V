b4cd01c41f12ffca28187f9928ec053f8f8a8743
Shodi <henrique.maeta@mjv.com.br>
2018-02-25 21:05:29 -0300

    Inital commit

---
+++ src/data-processing/csvObject.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+class CSVObject:
+    def __init__(self):
+        pass
---
+++ src/data-processing/processing.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import sys
+
+def main():
+    if not len(sys.argv):
+        print('Passe o nome do arquivo a ser processado.')
+        return
+    else:
+        print('oie')
+        return
+        
+if __name__ == '__main__':
+    main()

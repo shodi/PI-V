5dfe70bf5e383900acee240631a99a30744b773d
Aipi <tesouraet@gmail.com>
2018-02-28 02:16:30 -0300

    add remove outliers method

--- src/data_processing/csvObject.py
+++ src/data_processing/csvObject.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+
 class CSVObject:
     def __init__(self, do_have_headers):
         self.data = []
@@ -24,7 +25,7 @@ class CSVObject:
                     else:
                         null_qtty[str(index)] = [row_number]
                     continue
-                if self.statistic.has_key('sum_%s' % str(index)):
+                if 'sum_{}'.format(str(index)) in self.statistic:
                     self.statistic['sum_%s' % str(index)] += float(item)
                 else:
                     self.statistic['sum_%s' % str(index)] = float(item)
@@ -40,11 +41,28 @@ class CSVObject:
                     del line[index]
                     del self.statistic['sum_%s' % str(index)]
         self.calculate_statistics()
+
     def calculate_statistics(self):
         # TODO: Com base nos dados obtidos pelo método remove_null_columns
         # podemos calcular valores como a média, desvio padrão e a variância.
         # Tal atividade deverá ser implementada no método na qual este comentário reside.
         pass
+
+    def remove_outliers(self):
+        N = [6, 7, 15, 36, 39, 40, 41, 42, 43, 47, 200]
+        average = sum(N) / len(N)
+        q1 = N[round(0.25 * (len(N) + 1))]
+        # q2 = (N[int(len(N) / 2)] + N[int(len(N) / 2 - 1)]) / 2
+        # if len(N) % 2 == 0 else N[int(len(N) /2)]
+        q3 = N[round(0.75 * (len(N) + 1))]
+        IQR = q3 - q1
+        up_limit = average + IQR * 1.5
+        down_limit = average - IQR * 1.5
+
+        for value in N:
+            if value <= down_limit or value >= up_limit:
+                N.remove(value)
+
 # TODO: Implementar método que retira registros que ainda 
 # possuem dados nulos de colunas que não foram retiradas no método 
 # remove_null_columns

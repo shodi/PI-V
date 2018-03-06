20fd75e66924509dd17064219bc74ef090f1ac50
Shodi <henrique.maeta@mjv.com.br>
2018-03-06 08:44:35 -0300

    Adicionando notação de nulos

--- src/data_processing/processing.py
+++ src/data_processing/processing.py
@@ -4,8 +4,13 @@ import sys
 from .csvObject import CSVObject
 
 
-def init_process(file_name, headers=False):
+def init_process(file_name, headers, null_notation):
+    if null_notation is None:
+        null_notation = ['', None]
+    if headers is None:
+        headers = False
     csv_obj = CSVObject(headers, '%s.csv' % file_name)
+    csv_obj.set_null_notation(null_notation)
     csv_obj.remove_null_columns()
     csv_obj.data_normalization()
     csv_obj.generate_result('%s_result.csv' % file_name)

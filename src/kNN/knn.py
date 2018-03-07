9173b31e8c8aef5f3d0bc2359bcd9764ad9fd9be
Shodi <hikihenrique@yahoo.com>
2018-03-07 14:59:47 -0300

    Desenvolvimento do kNN

--- src/kNN/knn.py
+++ src/kNN/knn.py
@@ -9,32 +9,35 @@ class KNN:
         # a considerar
         self.k = k
 
-    def find_knn(self, neighbour_to_check, value):
-        a = self.__euclidean_distance(neighbour_to_check, value)
-        print(a)
-        return
+    def find_knn(self, data_set, value, metric=-1):
+        prediction = {'value': None}
+        for neighbour_to_check in data_set:
+            distance = self.__euclidean_distance(neighbour_to_check, value, metric)
+            if prediction.get('value') is None or distance < prediction.get('value'):
+                prediction['value'] = distance
+                prediction['class'] = neighbour_to_check[metric]
+        return prediction
 
-    def __euclidean_distance(self, p, q):
+    def __euclidean_distance(self, p, q, metric):
         # P e Q são instâncias de uma mesma classe
         sum = 0
         try:
-            if len(p) != len(q):
+            if len(p) != (len(q) + 1):
                 print('len: {}, len: {}'.format(len(p), len(q)))
                 raise ValueError('Inconsistência nos dados.')
-            for index, item in enumerate(p):
-                sum += (float(item) - float(q[index])) ** 2 
+            for index, item in enumerate(q):
+                sum += (float(item) - float(p[index])) ** 2
             return math.sqrt(sum)
         except Exception as error:
             raise error
 
 if __name__ == '__main__':
     knn_obj = KNN(3)
-    file_path = './../../resources/spreadsheets/result/abalone_result.csv'
-    plauzinho = ['0.2','0.3','0.4','0.5','0.6','0.7','0.8','0.9','0.01','0.0011']
+    file_path = './../../resources/spreadsheets/result/iris_result.csv'
+    plauzinho = ['0.2','0.3','0.4','0.5','0.6']
     try:
         with open(file_path, 'rb') as csv_file:
             lines = csv.reader(csv_file, delimiter=";")
-            for line in lines:
-                knn_obj.find_knn(line, plauzinho)
+            print(knn_obj.find_knn(lines, plauzinho))
     except Exception as error:
         print(error)

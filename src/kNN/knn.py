cb06f27f2104c9c1374ca3540e28f220b8f9feb8
Henrique Shodi Maeta <japa1996@hotmail.com>
2018-03-08 01:01:40 -0300

    Script kNN finalizado

--- src/kNN/knn.py
+++ src/kNN/knn.py
@@ -8,36 +8,59 @@ class KNN:
         # Quantidade de vizinhos mais próximos
         # a considerar
         self.k = k
+        self.kNN = None
 
-    def find_knn(self, data_set, value, metric=-1):
-        prediction = {'value': None}
+    def __build_obj(self, distance, obj_class):
+        return {'distance': distance, 'class': obj_class}
+
+    def get_prediction(self):
+        lesser_distance = None
+        for prediction in self.kNN:
+            if prediction['distance'] < lesser_distance \
+                or lesser_distance is None:
+                lesser_distance = prediction
+        return lesser_distance
+
+    def find_knn(self, data_set, value, metric=-1, skippable_indexes=[]):
+        prediction = []
         for neighbour_to_check in data_set:
-            distance = self.__euclidean_distance(neighbour_to_check, value, metric)
-            if prediction.get('value') is None or distance < prediction.get('value'):
-                prediction['value'] = distance
-                prediction['class'] = neighbour_to_check[metric]
+            distance = self.__euclidean_distance(neighbour_to_check, value, metric, skippable_indexes)
+            if len(prediction) < self.k:
+                obj = self.__build_obj(distance, neighbour_to_check[metric])
+                prediction.append(obj)
+                continue
+            for index in range(len(prediction)):
+                if prediction[index]['distance'] > distance:
+                    del prediction[index]
+                    obj = self.__build_obj(distance, neighbour_to_check[metric])
+                    prediction.append(obj)
+        self.kNN = prediction
         return prediction
 
-    def __euclidean_distance(self, p, q, metric):
+    def __euclidean_distance(self, p, q, metric, skippable_indexes):
         # P e Q são instâncias de uma mesma classe
         sum = 0
         try:
-            if len(p) != (len(q) + 1):
-                print('len: {}, len: {}'.format(len(p), len(q)))
-                raise ValueError('Inconsistência nos dados.')
+            # if len(p) != (len(skippable_indexes) + 1):
+            #     print('len: {}, len: {}'.format(len(p), len(q)))
+            #     raise ValueError('Inconsistência nos dados.')
             for index, item in enumerate(q):
+                if index in skippable_indexes or index == metric:
+                    continue
                 sum += (float(item) - float(p[index])) ** 2
             return math.sqrt(sum)
         except Exception as error:
             raise error
 
 if __name__ == '__main__':
-    knn_obj = KNN(3)
+    import sys
+    knn_obj = KNN(int(sys.argv[1]) if len(sys.argv) > 1 else 3)
     file_path = './../../resources/spreadsheets/result/iris_result.csv'
     plauzinho = ['0.2','0.3','0.4','0.5','0.6']
     try:
         with open(file_path, 'rb') as csv_file:
             lines = csv.reader(csv_file, delimiter=";")
-            print(knn_obj.find_knn(lines, plauzinho))
+            knn_obj.find_knn(lines, plauzinho)
+            print(knn_obj.get_prediction())
     except Exception as error:
         print(error)

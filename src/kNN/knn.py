6ba88266502c6e04fb4850416e65a3de1fa74d9b
Shodi <hikihenrique@yahoo.com>
2018-03-09 16:48:18 -0300

    knn finalizado

--- src/kNN/knn.py
+++ src/kNN/knn.py
@@ -9,17 +9,35 @@ class KNN:
         # a considerar
         self.k = k
         self.kNN = None
+        self.instance_qtd = 0 # quantidade de linhas do arquivo
 
     def __build_obj(self, distance, obj_class):
         return {'distance': distance, 'class': obj_class}
 
-    def get_prediction(self):
-        lesser_distance = None
+    def get_prediction(self):  #  chave   valor
+        class_occurrences = {} # {classe: Array<distancias>}
         for prediction in self.kNN:
-            if prediction['distance'] < lesser_distance \
-                or lesser_distance is None:
-                lesser_distance = prediction
-        return lesser_distance
+            if prediction['class'] not in class_occurrences:
+                class_occurrences[prediction['class']] = {}
+                class_occurrences[prediction['class']]['distances'] = [prediction['distance']]
+            else:
+                obj = class_occurrences[prediction['class']]['distances']
+                obj.append(prediction['distance'])
+        predicted = {'class': None, 'min_distance': -1, 'occurrences': -1}
+        for item in class_occurrences:
+            current_item = class_occurrences[item]
+            if len(current_item['distances']) > predicted['occurrences']:
+                predicted['class'] = float(item)
+                predicted['min_distance'] = min(current_item['distances'])
+                predicted['occurrences'] = len(current_item['distances'])
+            else:
+                if len(current_item['distances']) == predicted['occurrences']:
+                    current_min_distance = min(current_item['distances'])
+                    if predicted['min_distance'] > current_min_distance:
+                        predicted['class'] = float(item)
+                        predicted['min_distance'] = current_min_distance
+                        predicted['occurences'] = len(current_item['distances'])
+        return predicted
 
     def find_knn(self, data_set, value, metric=-1, skippable_indexes=[]):
         prediction = []
@@ -57,10 +75,10 @@ if __name__ == '__main__':
     knn_obj = KNN(int(sys.argv[1]) if len(sys.argv) > 1 else 3)
     file_path = './../../resources/spreadsheets/result/iris_result.csv'
     plauzinho = ['0.2','0.3','0.4','0.5','0.6']
-    try:
-        with open(file_path, 'rb') as csv_file:
-            lines = csv.reader(csv_file, delimiter=";")
-            knn_obj.find_knn(lines, plauzinho)
-            print(knn_obj.get_prediction())
-    except Exception as error:
-        print(error)
+    # try:
+    with open(file_path, 'rb') as csv_file:
+        lines = csv.reader(csv_file, delimiter=";")
+        knn_obj.find_knn(lines, plauzinho)
+        print(knn_obj.get_prediction())
+    # except Exception as error:
+    #     print('DEU ERRO: {}'.format(error))

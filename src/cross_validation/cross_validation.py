dc4ea1e1743be19595095a99c85b972b69481ee2
Aipi <tesouraet@gmail.com>
2018-03-11 01:43:34 -0300

    Improving cross validation

--- src/cross_validation/cross_validation.py
+++ src/cross_validation/cross_validation.py
@@ -43,30 +43,45 @@ class CrossValidation(object):
 
         Todo:
         """
-        self.chunks = {}
+        # dividir o arquivo em pedaços
+        self.fold = {}
         row_qtty = sum(1 for row in self.data_set)
         aux = 1
         for index in range(0, row_qtty):
             if aux == k + 1:
                 aux = 1
-            if str(aux) not in self.chunks:
-                self.chunks[str(aux)] = []
-            self.chunks[str(aux)].append(self.data_set[index])
+            if str(aux) not in self.fold:
+                self.fold[str(aux)] = []
+            self.fold[str(aux)].append(self.data_set[index])
             aux += 1
-        test = self.chunks['1']
-        del self.chunks['1']
-        trainning = []
-        for key, sublist in self.chunks.iteritems():
-            if key == 0:
-                continue
-            else:
-                for line in sublist:
-                    trainning.append(line)
-        knn_obj = KNN(int(sys.argv[1]) if len(sys.argv) > 1 else 3, trainning)
-        knn_obj.find_knn(test[0])
-        print(knn_obj.get_prediction())
-        # print(knn_obj.get_prediction())
 
+        # comparar testes e trainamentos
+        aux = 1
+        test_number = {}
+        for index in range(0, row_qtty):
+            if aux == k + 1:
+                aux = 1
+            test = self.fold[str(aux)]
+            del self.fold[str(aux)]
+            trainning = []
+            for key, sublist in self.fold.iteritems():
+                if key == 0:
+                    continue
+                else:
+                    for line in sublist:
+                        trainning.append(line)
+            knn_obj = KNN(
+                int(sys.argv[1]) if len(sys.argv) > 1 else 3, trainning)
+            knn_obj.find_knn(test[0])
+            test_predict = knn_obj.get_prediction()
+            if test_predict == self.data_set[index][-1]:
+                if aux not in test_number:
+                    test_number[str(aux)] = 0
+                else:
+                    test_number[str(aux)] += 1  
+            aux += 1
+        import pdb; pdb.set_trace()
+        print test_number
 
 
 if __name__ == '__main__':

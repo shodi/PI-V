6f4ea1826a64f8b3af3dd96c4193920382d42993
Aipi <tesouraet@gmail.com>
2018-03-13 01:36:51 -0300

    Creating docstrings to explain code to the documentation

--- src/cross_validation/cross_validation.py
+++ src/cross_validation/cross_validation.py
@@ -2,30 +2,47 @@
 # -*- coding: utf-8 -*-
 import csv
 import sys
+import random
 sys.path.append('../kNN')
 from knn import KNN
 
 
 class CrossValidation(object):
+    """Técnica para avaliar a capacidade de generalização de um modelo.
+
+        Args:
+            file_name (str): Nome do arquivo gerado após o tratamento e
+                processamento dos dados.
+
+    """
+
     def __init__(self, file_name):
+        """
+        Atributos:
+            data_set (list): Iniciação da lista que receberá toda a massa de
+                dados a ser trabalhada.
+            __set_data: Chamada de execução do método para leitura do arquivo.
+
+        """
         self.data_set = []
         self.__set_data(file_name)
 
     def __set_data(self, file_name):
         """Método de leitura e armazenamento do arquivo a ser processado
 
-        Este método faz a leitura do arquivo para armazena-lo para que possa
-        ser processado pelos demais métodos.
-
-        Atributos:
-            self.data_set(list):
+        Este método faz a leitura do arquivo e o armazena através de uma função
+        anônima numa variável, para que possa ser processado sequencialmente
+        pelos demais métodos.
 
         Args:
-            file_name(str): Nome do arquivo a ser lido.
+            file_name (str): Nome do arquivo a ser lido.
 
-        Returns:
+        Atributos:
+            lines (_csv.reader): Contém toda os dados do arquivo passado
+                separado por linhas.
+            data_set (list): Armazena toda a massa de dados do
+                determinado arquivo passado como parâmetro.
 
-        Todo:
         """
         with open('./../../resources/spreadsheets/result/{}'.format(
                 file_name)) as csv_file:
@@ -33,22 +50,39 @@ class CrossValidation(object):
             [(lambda x: self.data_set.append(x))(line) for line in lines]
 
     def k_fold(self, k):
-        """
+        """Calcula a acurácia sobre os erros encontrados em k iterações.
+
+        O método de validação cruzada denominado k-fold consiste em dividir o
+        conjunto total de dados em k subconjuntos mutuamente exclusivos do
+        mesmo tamanho e, a partir disto, um subconjunto é utilizado para teste
+        e os k-1 restantes são utilizados para estimação dos parâmetros e
+        calcula-se a acurácia do modelo.
+
         Atributos:
+            fold (dict): Variável de chave-valor que armazena as k divisões do
+                arquivo. Onde chave é o número da divisão, que vai de 1 à k, e
+                o valor é uma matriz (i por j), o i são as linhas do arquivo e
+                j é as colunas.
+            line_qtty (int): Calcula a quantidade total de linhas no arquivo.
+            hits (dict): Estrutura de dados chave-valor usado para calcular a
+                quantidade de acertos em cada uma k rodadas.
+            test_fold (list): Armazena o conjunto de dados, 1/k, usado para a
+                rodada de teste da vez.
+            trainning (list):
 
         Args:
-            k(int): Quantidade de sublistas a ser dividido.
+            k (int): Quantidade de sublistas a ser dividido.
 
-        Returns:
-
-        Todo:
+        No Longer Returned:
         """
         # dividir o arquivo em pedaços
         self.fold = {}
-        row_qtty = sum(1 for row in self.data_set)
-        print row_qtty
+        line_qtty = sum(1 for line in self.data_set)
+        print("Total of data: {}".format(line_qtty))
+        print("...............")
+        # random.shuffle(self.data_set)
         aux = 1
-        for index in range(0, row_qtty):
+        for index in range(0, line_qtty):
             if aux == k + 1:
                 aux = 1
             if str(aux) not in self.fold:
@@ -80,13 +114,15 @@ class CrossValidation(object):
                         hits[str(index)] = 0
                     hits[str(index)] += 1
                 aux += 1
-            print(test_fold)
-            print("---------------------------------------------------------------")
-        print hits
+            avg_hits = float(hits[str(index)] * 100) / float(len(test_fold))
+            print("k fold: {}/10".format(index))
+            print("average of hits: {}%".format(avg_hits))
+            print("...")
+        print(hits)
 
 
 if __name__ == '__main__':
-    file_name = 'iris_result.csv'
+    file_name = 'adult_result.csv'
     # try:
     cv = CrossValidation(file_name)
     cv.k_fold(10)

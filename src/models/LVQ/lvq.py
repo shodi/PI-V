957c6c3331c43dff7b8dff7cf6666265cfe5e6c8
Henrique Shodi Maeta <japa1996@hotmail.com>
2018-03-28 01:26:11 -0300

    Avancando o lvq

--- src/models/LVQ/lvq.py
+++ src/models/LVQ/lvq.py
@@ -9,7 +9,10 @@ class LVQ(object):
         self.data_set = data_set
         self.data_set_size = data_set_size
         self.classes_qtd = classes_qtd
-        self.radius = radius
+        self.desvio_padrao = radius
+        # self.const_t1 = 1000 / math.log(self.desvio_padrao)
+        self.taxa_aprendizado = 0.1
+        self.decaimento_aprendizado = 1000
 
     @staticmethod
     def get_random_value(init=0.1, final=1):
@@ -20,7 +23,34 @@ class LVQ(object):
         matrix = [[LVQ.get_random_value() for i in range(length)] for i in range(length)]
         self.matrix = matrix
 
+    def get_best_matching_unit(self, data_set, row_test):
+        distances = []
+        for instance in data_set:
+            dist = self.euclidean_distance(instance, row_test, 5, [5])
+            distances.append((instance, dist))
+        distances.sort(key=lambda distance: distance[1])
+        return distances[0][0]
+
+    def train(self, epochs=10):
+        self.get_initial_matrix(10)
+        for epoch in range(epochs):
+            rate = self.taxa_aprendizado * (1.0 - (epoch/float(epochs)))
+            sum_error = 0.0
+            for row in self.data_set:
+                bmu = self.get_best_matching_unit(self.matrix, row)
+                for i in range(len(row) - 1):
+                    error = row[i] - bmu[i]
+                    sum_error += error**2
+                    if bmu[-1] == row[-1]:
+                        bmu[i] = bmu[i] + rate * (row[i] - bmu[i])
+                    else:
+                        bmu[i] = bmu[i] - rate * (row[i] - bmu[i])
+            print('>iteracao=%d, taxa=%.3f, error=%.3f' % (epoch, rate, sum_error))
+        return self.matrix
+
+
     def euclidean_distance(self, p, q, metric, skippable_indexes):
+        _sum = 0
         for index, item in enumerate(q):
             if index in skippable_indexes or index == metric:
                 continue
@@ -42,5 +72,5 @@ if __name__ == '__main__':
                 data_set.append(aux)
                 data_set_size += 1
             lvq = LVQ(data_set, data_set_size, 3, 1)
-            print(lvq.get_initial_matrix(10))
+            lvq.train()
 

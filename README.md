667da34a1b73ce368ed07ed5a0670aaa83eab8a5
shodi <hikihenrique@yahoo.com>
2018-05-05 17:13:19 -0300

    Capturando audio pelo microfone

--- README.md
+++ README.md
@@ -4,10 +4,9 @@ PI machine learning
 Para que você possa executar o programa corretamente é necessário que você possua os seguintes pré requisitos:
   * SO Linux ou iOS
   * Python2.7 (Versões anteriores podem ocasionar erro de importação de submódulos)
-  * f2py e gfortran para compilar o código Fortran
-  * gfortran ou algum outro compilador compativel com o f2py
   * Algum compilador LaTeX para gerar o relatório da aplicação
+  * Possuir a lib libasound2-dev
+  * Possuir a lib portaudio19-dev
 
 ### Como executar
-para processar os dados contidos nas planilhas (resources/spreadsheets/*)
-basta executar o comando `python process.py` na raiz do projeto.
+Criar o ambiente virtual (vide venv), e instalar as dependências. Executar `pip install -r requirements.txt` na raiz do projeto.
---
+++ listener.py
@@ -0,0 +1,32 @@
+import alsaaudio, time, audioop
+import matplotlib.pyplot as plt
+import struct
+import numpy as np
+
+inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL)
+
+CHUNCK = 160
+BYTE = 4
+# Set attributes: Dual, 44100 Hz, 16 bit little endian samples
+inp.setchannels(2)
+inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+inp.setperiodsize(CHUNCK)
+
+fig, ax = plt.subplots()
+
+x = np.arange(0, BYTE * CHUNCK, 1)
+line, = ax.plot(x, np.random.rand(BYTE * CHUNCK))
+ax.set_ylim(0, 255)
+ax.set_xlim(0, CHUNCK)
+while True:
+    # Read data from device
+    length, data = inp.read()
+    if length:
+        # Return the maximum of the absolute value of all samples in a fragment.
+        # print(audioop.max(data, 2))
+        data_int = np.fromstring(data, dtype='b')
+        # data_int = np.array(struct.unpack(str(BYTE * CHUNCK) + 'B', data), dtype='b')
+        line.set_ydata(data_int)
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+    time.sleep(.001)

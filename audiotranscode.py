0fb2fcf8f3d794e412e0d6e1cf6e0b3671469941
Aipi <dmoniz@nice.br>
2018-05-08 12:02:33 -0300

    Melhorado código de transcodificador de arquivos de áudio

--- audiotranscode.py
+++ audiotranscode.py
@@ -1,15 +1,32 @@
 import subprocess
 import re
-# import os
+import os
 
 subprocess.call(['./ffmpeg_verify.sh'])
 
+wav_directory = './audios/wav/'
+original_directory = './audios/original/'
+
+if not os.path.exists(wav_directory):
+    os.makedirs(wav_directory)
+
+if not os.path.exists(original_directory):
+    os.makedirs(original_directory)
+
 folder = subprocess.check_output(['ls', './audios']).split('\n')
-new_name = []
-for file in folder:
-    new_name = re.sub('(.mpeg|.mp4|.ogg)$', '.wav', file)
+
+new_file = []
+
+for old_file in folder:
+    new_file = re.sub('(.mpeg|.mp4|.ogg)$', '.wav', old_file)
+
     subprocess.call(
-        'ffmpeg -i {0} -c:a pcm_f32le {1}'.format(file, new_name),
+        'ffmpeg -i {0} -c:a pcm_f32le {1}'.format(old_file, new_file),
         shell=True,
         cwd='./audios')
-    # os.remove('./audios/{0}'.format(file))
+
+    if not old_file == 'original':
+        os.rename('./audios/{0}'.format(new_file),
+                  '{0}{1}'.format(wav_directory, new_file))
+        os.rename('./audios/{0}'.format(old_file),
+                  '{0}{1}'.format(original_directory, old_file))

84ab50e25f7918cb7bea282123e0c7ef839f2d7c
Aipi <tesouraet@gmail.com>
2018-05-13 17:25:13 -0300

    Consumindo mais algumas informações

--- listener.py
+++ listener.py
@@ -9,9 +9,16 @@ import contextlib
 class Listener(object):
     """Get essencial informations from an audio file and save into CSV file.
 
+        Note:
+            It is taking so much time to calculate the mode
+            >>> self.mode = mode(self.audio_data)
+            ModeResult(mode=array([[-29, -29]],
+                       dtype=int16),
+                       count=array([[2074, 2074]]))
+
         Example:
-            >>> from listener import Listener
-            >>> listener = Listener('./audios/wav/mulher_8.wav')
+            from listener import Listener
+            listener = Listener('./audios/wav/mulher_8.wav')
             <listener.Listener object at 0x105ccc350>
 
         Args:
@@ -22,10 +29,17 @@ class Listener(object):
                 get_audio_data funtion.
     """
     def __init__(self, file_name):
-        self.label = 'homem' if 'homem' in file_name else 'mulher'
-        self.duration = self.get_duration(file_name)
         self.audio_data = self.get_audio_data(file_name)  # file name example
-        self.mean = self.get_mean(self.audio_data)
+        self.duration = self.get_duration(file_name)
+        self.iq1 = self.audio_data[int(0.25 * len(self.audio_data))]
+        self.iq3 = self.audio_data[int(0.75 * len(self.audio_data))]
+        self.label = 'homem' if 'homem' in file_name else 'mulher'
+        self.maxfun = fft(self.audio_data).max()
+        self.maxfun = self.audio_data.max()
+        self.meanfreq = self.get_mean_frequency(self.audio_data)
+        self.meanfun = self.get_mean_fundamental_frequency(self.audio_data)
+        self.minfun = fft(self.audio_data).min()
+        self.minfreq = self.audio_data.min()
 
     def get_audio_data(self, file_name):
         """Get data from an audio file with .wav extension.
@@ -42,17 +56,6 @@ class Listener(object):
         """
         rate, data = wav.read(file_name)
         return data
-
-    def get_mean(self, audio_data):
-        """Get the mean of audio data in Hz
-
-        Args:
-            audio_data(array): Data read from wav file
-
-        Return:
-            Average from data read from wav file
-        """
-        return sum(audio_data) / len(audio_data)
 
     def get_duration(self, file_name):
         """Get the duration of audio in seconds
@@ -74,10 +77,38 @@ class Listener(object):
             duration = frames / float(rate)
             return duration
 
+    def get_mean_frequency(self, audio_data):
+        """Get the mean of audio frequency in Hz
 
-'''fft_out = fft(data)
-import pdb; pdb.set_trace()
-print(fft_out)
-plt.plot(np.abs(fft_out))
-# plt.plot(data)
-plt.show()'''
+        Args:
+            audio_data(array): Data read from wav file
+
+        Return:
+            Average from data read from wav file
+        """
+        return sum(audio_data) / len(audio_data)
+
+    def get_mean_fundamental_frequency(self, audio_data):
+        """Get the mean of audio fundamental frequency in Hz
+
+        Args:
+            audio_data(array): Data read from wav file
+
+        Return:
+            Average from fundamental frequency
+
+        Todo:
+            Verify if it is correct.
+        """
+        return sum(fft(audio_data)) / len(fft(audio_data))
+
+    def save_into_csv(self):
+        """Function to save information from audio to .csv file
+
+        Args:
+            All informations required
+
+        Todo:
+            Make this function
+        """
+        pass

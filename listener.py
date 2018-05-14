4a7cde3924b4fa2a55e13eff9f0776db7e033caa
Aipi <tesouraet@gmail.com>
2018-05-14 00:49:34 -0300

    melhorado maneira de pegar alguns dados

--- listener.py
+++ listener.py
@@ -1,7 +1,7 @@
 # import matplotlib.pyplot as plt
 from scipy.io import wavfile as wav
 from scipy.fftpack import fft
-# import numpy as np
+import numpy as np
 import wave
 import contextlib
 
@@ -17,8 +17,8 @@ class Listener(object):
                        count=array([[2074, 2074]]))
 
         Example:
-            from listener import Listener
-            listener = Listener('./audios/wav/mulher_8.wav')
+from listener import Listener
+listener = Listener('./audios/wav/mulher_8.wav')
             <listener.Listener object at 0x105ccc350>
 
         Args:
@@ -29,17 +29,19 @@ class Listener(object):
                 get_audio_data funtion.
     """
     def __init__(self, file_name):
-        self.audio_data = self.get_audio_data(file_name)  # file name example
+        self.audio_data = self.get_audio_data(file_name)
         self.duration = self.get_duration(file_name)
-        self.iq1 = self.audio_data[int(0.25 * len(self.audio_data))]
-        self.iq3 = self.audio_data[int(0.75 * len(self.audio_data))]
+        self.iq1 = np.percentile(self.audio_data, q=25)
+        self.iq3 = np.percentile(self.audio_data, q=75)
+        self.median = np.percentile(self.audio_data, q=50)
         self.label = 'homem' if 'homem' in file_name else 'mulher'
-        self.maxfun = fft(self.audio_data).max()
-        self.maxfun = self.audio_data.max()
-        self.meanfreq = self.get_mean_frequency(self.audio_data)
-        self.meanfun = self.get_mean_fundamental_frequency(self.audio_data)
-        self.minfun = fft(self.audio_data).min()
-        self.minfreq = self.audio_data.min()
+        self.maxfun = np.amax(fft(self.audio_data))
+        self.maxfreq = np.amax(self.audio_data)
+        self.meanfreq = np.mean(self.audio_data)
+        self.meanfun = np.mean(fft(self.audio_data))
+        self.minfun = np.amin(fft(self.audio_data))
+        self.minfreq = np.amin(self.audio_data)
+        self.peak = self.get_peak_frequency(file_name)
 
     def get_audio_data(self, file_name):
         """Get data from an audio file with .wav extension.
@@ -77,30 +79,33 @@ class Listener(object):
             duration = frames / float(rate)
             return duration
 
-    def get_mean_frequency(self, audio_data):
-        """Get the mean of audio frequency in Hz
-
-        Args:
-            audio_data(array): Data read from wav file
-
-        Return:
-            Average from data read from wav file
+    def get_peak_frequency(self, file_name):
         """
-        return sum(audio_data) / len(audio_data)
-
-    def get_mean_fundamental_frequency(self, audio_data):
-        """Get the mean of audio fundamental frequency in Hz
-
-        Args:
-            audio_data(array): Data read from wav file
-
-        Return:
-            Average from fundamental frequency
-
-        Todo:
-            Verify if it is correct.
+        https://stackoverflow.com/questions/37813059/i-am-getting-peak-
+        frequency-from-wav-file-but-for-recorded-2-channels-wav-it-is#answer-37855872
         """
-        return sum(fft(audio_data)) / len(fft(audio_data))
+        fname = file_name
+        wav_file = wave.open(fname, 'r')
+        frate = wav_file.getframerate()
+        data_size = wav_file.getnframes()
+        data = wav_file.readframes(data_size)
+        nChannels = wav_file.getnchannels()
+        nSample = wav_file.getsampwidth()
+        data_size = data_size * nChannels * nSample
+        wav_file.close()
+        if nSample == 2:
+            fmt = "<i2"
+        else:
+            fmt = "<i4"
+        data = np.frombuffer(data, dtype=fmt)
+        if nChannels == 2:
+            data = data.reshape(-1, nChannels)
+            data = data.sum(axis=1) / 2
+        # and now the same way as above as said by maxpowers
+        freq_nq = len(data) // 2
+        x = abs(np.fft.fft(data))[:freq_nq] / len(data) * 2
+        freqs = np.fft.fftfreq(len(data), 1. / frate)[:freq_nq]
+        return freqs[np.argmax(x)]
 
     def save_into_csv(self):
         """Function to save information from audio to .csv file

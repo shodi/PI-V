03d75765d6a816b8f810790106c1ff4cf6a8423e
Aipi <tesouraet@gmail.com>
2018-05-13 22:46:55 -0300

    add requirements.txt

--- listener.py
+++ listener.py
@@ -1,7 +1,7 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from scipy.io import wavfile as wav
 from scipy.fftpack import fft
-import numpy as np
+# import numpy as np
 import wave
 import contextlib
 

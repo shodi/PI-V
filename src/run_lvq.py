937496a09bc10bc6a0d31cc505975731a047b602
shodi <hikihenrique@yahoo.com>
2018-05-18 01:07:42 -0300

    preparando para chamar o lvq de dentro do cross validation

--- src/run_lvq.py
+++ src/run_lvq.py
@@ -1,14 +1,11 @@
 import csv
 from LVQ import lvq
-from sklearn.model_selection import cross_validate
-from sklearn import datasets, linear_model
+from cross_validation import CrossValidation
 
 if __name__ == '__main__':
-    files = ['iris', 'abalone', 'adult', 'breast-cancer', 'wine', 'winequality-red', 'winequality-white']
-    for file_name in files:
-        with open('../resources/spreadsheets/result/%s_result.csv' % file_name) as csv_file:
-            lines = csv.reader(csv_file, delimiter=';')
-            lasso = linear_model.Lasso()
-            data = []
-            [(lambda x: data.append(x))(line) for line in lines]
-            cv_result = cross_validate(lasso, data, y=data, cv=10)
+    suffix = '_result.csv'
+    path = '../resources/spreadsheets/result/'
+    files = ['iris', 'abalone', 'breast-cancer', 'wine', 'winequality-red', 'winequality-white', 'adult']
+    for file in files:
+        with open(path + file + suffix) as csv:
+            print('abriu')
